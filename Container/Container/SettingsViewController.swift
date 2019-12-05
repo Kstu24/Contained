e18d86e2ca633f25c109dc012d5e575c8ab97c56
Kevin Stewart <kistewart419@gmail.com>
2019-12-04 23:43:18 -0500

    Build the Settings Screen and Build the Game Screen

--- Container/Container/SettingsViewController.swift
+++ Container/Container/SettingsViewController.swift
@@ -9,11 +9,21 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+        Settings.shared.shouldRoll = sender.isOn
+        
+    }
+    
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+        Settings.shared.shouldZoom = sender.isOn
     }
     
 
